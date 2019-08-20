ddb2cdd3df92d5ccb64b64b6cfb0c58deadf33d3
Shoroh777 <Shoroh777@users.noreply.github.com>
2019-08-20 14:42:57 +0300

    default done

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,142 @@
 const gulp = require('gulp'),
-    rename = require('gulp-rename'),
-    scss = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync').create();
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
 
-css_style = (done) => {
-
-    gulp.src('devApp/scss/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(scss({
-            errorLogToConsole: true,
-            outputStyle: 'compressed'
-        }))
-        .on('error', console.error.bind(console))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write('./'))
-        .pipe( gulp.dest('build/css/'))
-        .pipe(browserSync.stream());
-
-    done()
+const path = {
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: './build/',
+        js: './build/js/',
+        css: './build/css/',
+        img: './build/img/',
+        fonts: './build/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'src/components/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: 'src/styles/main.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'src/**/*.html',
+        js: 'src/components/**/*.js',
+        style: 'src/components/**/*.scss',
+        styleMain: 'src/styles/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build'
 };
 
-sync = (done) => {
-    browserSync.init({
-        server: {
-            baseDir: 'build/'
-        },
-        port: 4550
-    });
+const config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 4550,
+    logPrefix: "Shoroh_Server"
+};
+
+buildHtml = (done) => {
+    gulp.src(path.src.html) //Выберем файлы по нужному пути
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
 
     done();
 };
 
-browserReload = (done) => {
-    browserSync.reload();
+ buildJs = (done) => {
+    gulp.src(path.src.js) //Найдем наш main файл
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(sourcemaps.init()) //Инициализируем sourcemap
+        .pipe(uglify()) //Сожмем наш js
+        .pipe(sourcemaps.write()) //Пропишем карты
+        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+
+     done();
+};
+
+buildStyles = (done) => {
+    gulp.src(path.src.style) //Выберем наш main.scss
+        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(sass({
+            errorLogToConsole: true,
+            outputStyle: 'compressed'
+        }))
+        .on('error', console.error.bind(console))
+        .pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(cssmin()) //Сожмем
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(reload({stream: true}));
+
+    done();
+};
+
+buildImage = (done) => {
+    gulp.src(path.src.img) //Выберем наши картинки
+        .pipe(imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img)) //И бросим в build
+        .pipe(reload({stream: true}));
+
+    done();
+};
+
+buildFonts = (done) => {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts));
 
     done();
 };
 
 watchFiles = () => {
-    gulp.watch('devApp/scss/**/*', css_style);
-    gulp.watch('devApp/**/*.html', browserReload);
-    gulp.watch('devApp/**/*.js', browserReload);
+    gulp.watch(path.watch.html, buildHtml);
+    gulp.watch(path.watch.js, buildJs);
+    gulp.watch(path.watch.style, buildStyles);
+    gulp.watch(path.watch.styleMain, buildStyles);
+    gulp.watch(path.watch.img, buildImage);
+    gulp.watch(path.watch.fonts, buildFonts);
 };
 
-gulp.task('default', gulp.parallel(watchFiles, sync, css_style));
+gulp.task('html:build', buildHtml);
+gulp.task('js:build', buildJs);
+gulp.task('style:build', buildStyles);
+gulp.task('image:build', buildImage);
+gulp.task('fonts:build', buildFonts);
+
+gulp.task('build', gulp.parallel(
+    buildHtml,
+    buildJs,
+    buildStyles,
+    buildFonts,
+    buildImage
+));
+
+gulp.task('watch', watchFiles);
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('default', gulp.parallel('build', 'webserver', 'watch'));
